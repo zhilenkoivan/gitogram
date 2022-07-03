2c840473ef2c9840ba5c55977223c25425d0c5fb
Ivan Zhilenko <zhilenkovany1@gmail.com>
2022-07-03 18:57:52 +0300

    second change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/gitogram/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gitogram/'
+    : '/'
 })
